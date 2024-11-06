6f9a4f3c084a00eb2e023e0abce4195c62cff54e
Tringa <95575981+TringaI@users.noreply.github.com>
2024-11-06 19:37:49 +0100

    loading

--- src/Pages/Shop/Index.jsx
+++ src/Pages/Shop/Index.jsx
@@ -15,14 +15,26 @@ let images = importAllImages(
 
 function Index() {
     const [isClient, setIsClient] = useState(false);
+    const [loading, setLoading] = useState(true);
     const navigate = useNavigate();
 
     useEffect(() => {
         setIsClient(true);
+        const imageLoadPromises = images.map(
+            ({ image }) =>
+                new Promise((resolve) => {
+                    const img = new Image();
+                    img.src = image;
+                    img.onload = resolve;
+                    img.onerror = resolve;
+                })
+        );
+
+        Promise.all(imageLoadPromises).then(() => setLoading(false));
     }, []);
 
     const handleBuyItemClick = (filename) => {
-        navigate(`/${filename}`); // Navigate to ItemDetail page with filename
+        navigate(`/${filename}`);
     };
 
     return (
@@ -30,17 +42,17 @@ function Index() {
             <div className="w-[80%] flex flex-col justify-center items-center">
                 <h1 className="mt-40 text-[20px] md:text-[35px]">Filomena's Flowers <b className="font-medium">Online Shop</b></h1>
                 <div className="hr-shop"></div>
-                {isClient && images ? (
-                    <div className="w-full grid grid-cols-12 md:gap-5 lg:gap-20  mt-10 ">
+                {loading ? (
+                    <div className="loading-spinner"></div>
+                ) : (
+                    <div className="w-full grid grid-cols-12 md:gap-5 lg:gap-20 mt-10">
                         {images.map(({ filename, image }, index) => (
                             <div className="col-span-12 md:col-span-4 mt-10 md:mt-0 lg:col-span-3 flex flex-col shop-items-containers" key={index}>
                                 <img src={`/products/${filename}`} className="w-full" alt="Product" />
-                                <button className=" shop-buttons" onClick={() => handleBuyItemClick(filename)}>Order Now</button>
+                                <button className="shop-buttons" onClick={() => handleBuyItemClick(filename)}>Order Now</button>
                             </div>
                         ))}
                     </div>
-                ) : (
-                    <p>Loading</p>
                 )}
             </div>
         </div>
